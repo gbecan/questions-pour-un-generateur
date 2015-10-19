da28c3307339c36cca0ad6547088facbb498aa46
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-10-19 15:07:18 +0200

    play random audio on demand

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,11 +7,15 @@ import play.api.mvc._
 
 class Application extends Controller {
 
-  val pattern = Repeat(Sequence(
-      Question("Q1", "Q2"),
-      Repeat(Buzzer("B1", "B2"), 2, 4),
-      Answer("A1", "A2")
-    ), 2, 2)
+//  val pattern = Repeat(
+//    Sequence(
+//      Question("Q1", "Q2"),
+//      Repeat(Buzzer("B1", "B2"), 2, 4),
+//      Answer("A1", "A2")
+//  ), 2, 2)
+
+  val path = "/assets/audio/"
+  val pattern = Question(path + "test0.mp3", path + "test1.mp3", path + "test2.mp3")
 
   def index = Action {
     Ok(views.html.index())
--- public/javascripts/GeneratorCtrl.js
+++ public/javascripts/GeneratorCtrl.js
@@ -1,12 +1,19 @@
 angular.module("JLApp").controller("GeneratorCtrl", function($scope, $http) {
 
+    var player = document.getElementById("player");
+    var playerSource = player.getElementsByTagName("source")[0];
+
     $scope.variant = [];
+
 
     $scope.generate = function() {
         $http
             .get("/generate")
             .then(function(response) {
                 $scope.variant = response.data;
+                playerSource.src = $scope.variant[0];
+                player.load();
+                player.play();
             }, function(error){
                 console.error(error);
             })
